2694da69fe65299624086dd51e51c2d88a11939a
DESKTOP-HET\HP <het.shah0207@gmail.com>
2025-03-30 11:42:44 +0530

    Project Structure Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+   name='Generative AI Project',
+   version='0.0.0',
+   author='Het Shah',
+   author_email='het.shah0207@gmail.com',
+   packages=find_packages(),
+   install_requires = []
+)
